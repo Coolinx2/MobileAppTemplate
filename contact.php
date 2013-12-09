4c1bb65f4788049135d1a9f358e8c2e06c99d307
Colin Thomson <colin72@live.co.uk>
2013-12-09 07:12:18 +0000

    First commit template

---
+++ contact.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<!--
+Coolin contact page.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        echo "PHP code being compiled okay."
+        ?>
+        <br/>
+        <a href="index.html">Back</a>
+    </body>
+</html>
